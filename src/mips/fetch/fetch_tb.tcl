09976b5875b4abbf5cec78d96d4d3315be1f41e0
chrismartel <christian.martel2@mail.mcgill.ca>
2022-04-13 16:56:21 -0400

    updated fetch tb and added tcl script

---
+++ src/mips/fetch/fetch_tb.tcl
@@ -0,0 +1,33 @@
+
+;# add waves to the Wave window
+
+proc AddWaves {} {
+	add wave -position end  sim:/fetch_tb/I_clk
+	add wave -position end  sim:/fetch_tb/I_reset
+	add wave -position end  sim:/fetch_tb/I_en
+	add wave -position end  sim:/fetch_tb/I_stall
+	add wave -position end  sim:/fetch_tb/I_jump
+	add wave -position end  sim:/fetch_tb/I_branch
+	add wave -position end  sim:/fetch_tb/I_pc_branch
+	add wave -position end  sim:/fetch_tb/I_mem_instruction
+	add wave -position end  sim:/fetch_tb/I_waitrequest
+	add wave -position end  sim:/fetch_tb/O_updated_pc
+	add wave -position end  sim:/fetch_tb/O_instruction_address
+	add wave -position end  sim:/fetch_tb/O_instruction
+	add wave -position end  sim:/fetch_tb/O_memread
+}
+
+vlib work
+
+;# compile components
+vcom fetch.vhd
+vcom fetch_tb.vhd
+
+;# start simulation
+vsim -t ps work.fetch_tb
+
+;# add the waves
+AddWaves
+
+;# run for 2 us
+run 2 us
--- src/mips/fetch/fetch_tb.vhd
+++ src/mips/fetch/fetch_tb.vhd
@@ -1,3 +1,4 @@
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -10,91 +11,97 @@ constant clk_period : time := 10 ns;
 
 component fetch is
 	port (
-        clk: in std_logic;
-        reset: in std_logic;
-        stall: in std_logic;
+        I_clk: in std_logic;
+        I_reset: in std_logic;
+	I_en: in std_logic;
+        I_stall: in std_logic;
     
-        -- jump flag
-        jump: in std_logic;
-        -- branch flag 
-        branch: in std_logic; 
-        -- incase of a branch or a jump use this
-        pc_branch: in std_logic_vector (31 downto 0); 
+        -- I_jump flag
+        I_jump: in std_logic;
+        -- I_branch flag 
+        I_branch: in std_logic; 
+        -- incase of a I_branch or a I_jump use this
+        I_pc_branch: in std_logic_vector (31 downto 0); 
         
         -- Memory Inputs:
-        mem_instruction : in std_logic_vector (31 downto 0);
-        waitrequest: in std_logic;
+        I_mem_instruction : in std_logic_vector (31 downto 0);
+        I_waitrequest: in std_logic;
     
         -- Outputs for fetch unit
-        pc_updated: out std_logic_vector (31 downto 0);
-        instruction_address: out std_logic_vector (31 downto 0);
-        memread: out std_logic
+	O_updated_pc: out std_logic_vector (31 downto 0);
+	O_instruction_address: out INTEGER RANGE 0 TO 32768-1;
+	O_memread: out std_logic;
+	O_instruction : out std_logic_vector (31 downto 0)
 	);
 end component;
 	
-signal clk : std_logic := '0';
-signal reset : std_logic := '0';
-signal stall : std_logic := '0';
-signal jump : std_logic := '0';
-signal branch : std_logic := '0';
-signal pc_branch : std_logic_vector (31 downto 0);
-signal mem_instruction : std_logic_vector (31 downto 0);
-signal waitrequest : std_logic := '1';
-signal pc_updated : std_logic_vector (31 downto 0);
-signal instruction_address : std_logic_vector (31 downto 0);
-signal memread : std_logic := '0';
+signal I_clk : std_logic := '0';
+signal I_reset : std_logic := '0';
+signal I_en: std_logic := '0';
+signal I_stall : std_logic := '0';
+signal I_jump : std_logic := '0';
+signal I_branch : std_logic := '0';
+signal I_pc_branch : std_logic_vector (31 downto 0);
+signal I_mem_instruction : std_logic_vector (31 downto 0);
+signal I_waitrequest : std_logic := '1';
+signal O_updated_pc : std_logic_vector (31 downto 0);
+signal O_instruction_address : INTEGER RANGE 0 TO 32768-1;
+signal O_instruction : std_logic_vector (31 downto 0);
+signal O_memread : std_logic := '0';
 
 begin
 
 dut_fetch: fetch
 port map(
-        clk => clk,
-        reset => reset,
-        stall => stall,
-        jump => jump,
-        branch => branch,
-        pc_branch => pc_branch,
-        mem_instruction => mem_instruction,
-        waitrequest => waitrequest,
-        pc_updated => pc_updated,
-        instruction_address => instruction_address,
-        memread =>  memread
+        I_clk => I_clk,
+        I_reset => I_reset,
+	I_en => I_en,
+        I_stall => I_stall,
+        I_jump => I_jump,
+        I_branch => I_branch,
+        I_pc_branch => I_pc_branch,
+        I_mem_instruction => I_mem_instruction,
+        I_waitrequest => I_waitrequest,
+        O_updated_pc => O_updated_pc,
+        O_instruction_address => O_instruction_address,
+        O_memread =>  O_memread
 );
 
 
 clk_process : process
 begin
-  clk <= '0';
+  I_clk <= '0';
   wait for clk_period/2;
-  clk <= '1';
+  I_clk <= '1';
   wait for clk_period/2;
 end process;
 
 test_process : process
 begin
-	stall <= '0'; -- assume there is no stall
-	jump <= '0';	--  assume there is no jump
-	branch <= '0'; -- assume there is no branch
-    waitrequest <= '0'; -- assume there is no wait
+	I_en <= '1';
+	I_stall <= '0'; -- assume there is no I_stall
+	I_jump <= '0';	--  assume there is no I_jump
+	I_branch <= '0'; -- assume there is no I_branch
+    	I_waitrequest <= '0'; -- assume there is no wait
 
 	-- Test case 1: Fetch instruction pc = 0
-    report "----- Test 1: fetch pc = 0 -----";
-	pc_branch <= std_logic_vector( to_unsigned( 5, pc_branch'length));
-	branch <= '0';
-    wait for clk_period;
-	assert unsigned(pc_updated) = 4 report "updated pc test 1 is wrong" severity error;
+    	report "----- Test 1: fetch pc = 0 -----";
+	I_pc_branch <= std_logic_vector( to_unsigned( 5, I_pc_branch'length));
+	I_branch <= '0';
+    	wait for clk_period;
+	assert unsigned(O_updated_pc) = 4 report "updated pc test 1 is wrong" severity error;
 
-    report "----- Test 2: fetch pc = 4 -----";
-	pc_branch <= std_logic_vector( to_unsigned( 5, pc_branch'length));
-	branch <= '0';
-    wait for clk_period;
-	assert unsigned(pc_updated) = 8 report "updated pc test 2 is wrong" severity error;
+    	report "----- Test 2: fetch pc = 4 -----";
+	I_pc_branch <= std_logic_vector( to_unsigned( 5, I_pc_branch'length));
+	I_branch <= '0';
+    	wait for clk_period;
+	assert unsigned(O_updated_pc) = 8 report "updated pc test 2 is wrong" severity error;
 	
-    report "----- Test 3: fetch pc = 4 but pc branch = 5 -----";
-	pc_branch <= std_logic_vector(to_unsigned( 5, pc_branch'length));
-	branch <= '1';
-    wait for clk_period;
-	assert unsigned(pc_updated) = 5 report "pc branch test 3 is wrong" severity error;
+    	report "----- Test 3: fetch pc = 4 but pc I_branch = 5 -----";
+	I_pc_branch <= std_logic_vector(to_unsigned( 5, I_pc_branch'length));
+	I_branch <= '1';
+    	wait for clk_period;
+	assert unsigned(O_updated_pc) = 5 report "pc I_branch test 3 is wrong" severity error;
 	
     report "----- Confirming all tests have ran -----";
     wait;
