51f9f38e4dd1fc7ff3dbed5fe55a9b8d4d13eb92
lu-barb <55468973+lu-barb@users.noreply.github.com>
2022-04-15 16:14:02 -0400

    removed regs component for modularity

--- src/mips/writeback/Write_Back.vhd
+++ src/mips/writeback/Write_Back.vhd
@@ -7,63 +7,38 @@ port (I_regDwe : in std_logic;
 	I_mem_read : in std_logic;
 	I_alu : in std_logic_vector (31 downto 0);
 	I_mem: in std_logic_vector (31 downto 0);
-	I_reg_address: in std_logic_vector (4 downto 0);
-	I_clk : in std_logic;
-	I_reset : in std_logic;
-	I_rt : in std_logic_vector(4 downto 0);
-	I_rs: in std_logic_vector(4 downto 0);
-	O_datas: out std_logic_vector(31 downto 0);
-	O_datat: out std_logic_vector(31 downto 0)
+	I_rd: in std_logic_vector (4 downto 0);
+	I_jump : in std_logic;
+	I_branch: in std_logic;
+	
+	O_we : out std_logic;
+	O_rd: out std_logic_vector (4 downto 0);
+	O_mux : out std_logic_vector (31 downto 0);
   );
 end write_back;
 
 architecture write_back_arch of write_back is
-
-	component regs
-		port (I_clk: in std_logic;
-				I_reset: in std_logic;
-				I_en: in std_logic;
-				I_datad: in std_logic_vector(31 downto 0);
-				I_rt: in std_logic_vector(4 downto 0);
-				I_rs: in std_logic_vector(4 downto 0);
-				I_rd: in std_logic_vector(4 downto 0);
-				I_we: in std_logic;
-				O_datas: out std_logic_vector(31 downto 0);
-				O_datat: out std_logic_vector(31 downto 0)
-		); 
-	end component;
-	
-	signal O_mux : std_logic_vector (31 downto 0);
-	signal I_en : std_logic;
 	
 begin
-
-	regWrite : regs
-	port map(I_clk => I_clk,
-				I_reset => I_reset,
-				I_en => I_en,
-				I_datad => O_mux,
-				I_rt => I_rt,
-				I_rs => I_rs,
-				I_rd => I_reg_address,
-				I_we => I_regDwe,
-				O_datas => O_datas,
-				O_datat => O_datat
-	);
-	
 	process(I_clk)
 		begin
 			if rising_edge(I_clk) then
-				--mux for choosing input from ALU or MEM
-				if ((I_regDwe and I_mem_read) = '0') then
-					O_mux <= I_alu;
-				elsif ((I_regDwe and I_mem_read) = '1') then
-					O_mux <= I_mem;
+				if ((I_jump or I_branch) != '1') then
+					--mux for choosing input from ALU or MEM
+					if ((I_regDwe and I_mem_read) = '0') then
+						O_mux <= I_alu;
+					elsif ((I_regDwe and I_mem_read) = '1') then
+						O_mux <= I_mem;
+					else
+						O_mux <= "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+					end if;
+					O_rd <= I_rd;
+					O_we <= '1';
 				else
+					O_rd <= "XXXXX";
 					O_mux <= "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+					O_we <= '0';
 				end if;
-			
-				I_en <= '1';
 			end if;
 	end process;
 end write_back_arch;
