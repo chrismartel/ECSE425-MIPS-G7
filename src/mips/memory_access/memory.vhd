85cf1cc76b02b37ee904a183108fd5201ea70d1b
chrismartel <christian.martel2@mail.mcgill.ca>
2022-04-15 20:25:26 -0400

    removed unused signals

--- src/mips/memory_access/memory.vhd
+++ src/mips/memory_access/memory.vhd
@@ -32,12 +32,12 @@ port(
 	
 	
 	-- data_memory relevant signals
-	data_address: out integer range 0 to ram_size-1;
-	data_memread: out std_logic;
-	data_waitrequest: in std_logic;
-	data_writedata: out std_logic_vector(31 downto 0);
-	data_memwrite: out std_logic;
-	data_readdata: in std_logic_vector(31 downto 0);
+	I_data_waitrequest: in std_logic;
+	O_data_address: out integer range 0 to ram_size-1;
+	O_data_memread: out std_logic;
+	O_data_writedata: out std_logic_vector(31 downto 0);
+	O_data_memwrite: out std_logic;
+	-- data_readdata: in std_logic_vector(31 downto 0);
 	
 	-- Control Signals Outputs
 	O_rd: out std_logic_vector (4 downto 0); 			-- the destination register where to write the instr. result
@@ -48,10 +48,7 @@ port(
 	O_reg_write: out std_logic; 					-- indicates if value calculated in ALU must be written to destination register
 	
 	O_alu_result: out std_logic_vector(31 downto 0);
-	O_stall: out std_logic;
-	
-	O_forward_rd: out std_logic_vector (4 downto 0);
-	O_forward_mem_reg_write: out std_logic
+	O_stall: out std_logic
 );
 end memory;
 
@@ -61,7 +58,6 @@ architecture arch of memory is
 begin
 
 -- actual logic here
-
 
 -- check if opcode matches LW or SW
 -- if it does, do memory stuff
@@ -80,8 +76,6 @@ begin
 			O_reg_write <= '0';
 			O_alu_result <= (others =>'0');
 			O_stall <= '0';
-			O_forward_rd <= (others =>'0');
-			O_forward_mem_reg_write <= '0';
 			
 		-- synchronous clock active high
 		elsif I_clk'event and I_clk = '1' then
@@ -98,12 +92,10 @@ begin
 					
 					-- pass control signals to next stage
 					O_rd <= I_rd;
-					O_forward_rd <= I_rd;
 					O_mem_read <= I_mem_read;
 					O_mem_write <= I_mem_write;
 					O_branch <= I_branch;
 					O_reg_write <= I_reg_write;
-					O_forward_mem_reg_write <= I_reg_write;
 					O_jump <= I_jump;
 
 					
@@ -111,23 +103,23 @@ begin
 					-- check if I_mem_read is high, and performs a read operation
 					if I_mem_read = '1' then
 						-- fetch data from address I_alu_result
-						data_address <= to_integer(unsigned(std_logic_vector(I_alu_result)));
-						data_memread <= '1';
-						data_memwrite <= '0';
+						O_data_address <= to_integer(unsigned(std_logic_vector(I_alu_result)));
+						O_data_memread <= '1';
+						O_data_memwrite <= '0';
 
 					-- check if I_mem_write is high, and performs a write operation
 					elsif I_mem_write = '1' then
 						-- write I_rt_data to address I_alu_result
-						data_address <= to_integer(unsigned(std_logic_vector(I_alu_result)));
-						data_writedata <= I_rt_data;
-						data_memwrite <= '1';
-						data_memread <= '0';
+						O_data_address <= to_integer(unsigned(std_logic_vector(I_alu_result)));
+						O_data_writedata <= I_rt_data;
+						O_data_memwrite <= '1';
+						O_data_memread <= '0';
 
 					-- otherwise, simply passes stuff forward
 					else
 					O_alu_result <= I_alu_result;
-					data_memread <= '0';
-					data_memwrite <= '0';
+					O_data_memread <= '0';
+					O_data_memwrite <= '0';
 					-- end memread/write check
 					end if;
 				-- end stall = 1
