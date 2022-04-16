b42f5c3088f7c52ded25da8fd9fd458fbf7aebd1
BrunoMarcoux <bruno.marcoux@mail.mcgill.ca>
2022-04-15 20:26:16 -0400

    wip

--- src/mips/fetch/fetch_tb.vhd
+++ src/mips/fetch/fetch_tb.vhd
@@ -18,26 +18,25 @@ component fetch is
         I_reset: in std_logic;
 	I_en: in std_logic;
         I_stall: in std_logic;
-    
+
         -- I_jump flag
         I_jump: in std_logic;
-        -- I_branch flag 
-        I_branch: in std_logic; 
+        -- I_branch flag
+        I_branch: in std_logic;
         -- incase of a I_branch or a I_jump use this
-        I_pc_branch: in std_logic_vector (31 downto 0); 
-        
+        I_pc_branch: in std_logic_vector (31 downto 0);
+
         -- Memory Inputs:
         I_mem_instruction : in std_logic_vector (31 downto 0);
         I_waitrequest: in std_logic;
-    
+
         -- Outputs for fetch unit
 	O_updated_pc: out std_logic_vector (31 downto 0);
 	O_instruction_address: out INTEGER RANGE 0 TO 32768-1;
-	O_memread: out std_logic;
-	O_instruction : out std_logic_vector (31 downto 0)
+	O_memread: out std_logic
 	);
 end component;
-	
+
 signal I_clk : std_logic := '0';
 signal I_reset : std_logic := '0';
 signal I_en: std_logic := '0';
@@ -49,7 +48,6 @@ signal I_mem_instruction : std_logic_vector (31 downto 0);
 signal I_waitrequest : std_logic := '1';
 signal O_updated_pc : std_logic_vector (31 downto 0);
 signal O_instruction_address : INTEGER RANGE 0 TO 32768-1;
-signal O_instruction : std_logic_vector (31 downto 0);
 signal O_memread : std_logic := '0';
 
 begin
@@ -58,7 +56,7 @@ dut_fetch: fetch
 port map(
         I_clk => I_clk,
         I_reset => I_reset,
-	I_en => I_en,
+				I_en => I_en,
         I_stall => I_stall,
         I_jump => I_jump,
         I_branch => I_branch,
@@ -99,13 +97,13 @@ begin
 	I_branch <= '0';
     	wait for clk_period;
 	assert unsigned(O_updated_pc) = 8 report "updated pc test 2 is wrong" severity error;
-	
+
     	report "----- Test 3: fetch pc = 4 but pc I_branch = 5 -----";
 	I_pc_branch <= std_logic_vector(to_unsigned( 5, I_pc_branch'length));
 	I_branch <= '1';
     	wait for clk_period;
 	assert unsigned(O_updated_pc) = 5 report "pc I_branch test 3 is wrong" severity error;
-	
+
     report "----- Confirming all tests have ran -----";
     wait;
 end process;
