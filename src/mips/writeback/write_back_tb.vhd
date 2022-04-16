cc440d3510fae53a70d001e400d3bb0af51592dd
chrismartel <christian.martel2@mail.mcgill.ca>
2022-04-15 22:49:35 -0400

    fixed tcl for writeback

--- src/mips/writeback/write_back_tb.vhd
+++ src/mips/writeback/write_back_tb.vhd
@@ -1,42 +1,15 @@
--- Copyright (C) 2020  Intel Corporation. All rights reserved.
--- Your use of Intel Corporation's design tools, logic functions 
--- and other software and tools, and any partner logic 
--- functions, and any output files from any of the foregoing 
--- (including device programming or simulation files), and any 
--- associated documentation or information are expressly subject 
--- to the terms and conditions of the Intel Program License 
--- Subscription Agreement, the Intel Quartus Prime License Agreement,
--- the Intel FPGA IP License Agreement, or other applicable license
--- agreement, including, without limitation, that your use is for
--- the sole purpose of programming logic devices manufactured by
--- Intel and sold by Intel or its authorized distributors.  Please
--- refer to the applicable agreement for further details, at
--- https://fpgasoftware.intel.com/eula.
-
--- ***************************************************************************
--- This file contains a Vhdl test bench template that is freely editable to   
--- suit user's needs .Comments are provided in each section to help the user  
--- fill out necessary details.                                                
--- ***************************************************************************
--- Generated on "04/15/2022 16:41:17"
-                                                            
--- Vhdl Test Bench template for design  :  write_back
--- 
--- Simulation tool : ModelSim-Altera (VHDL)
--- 
-
 LIBRARY ieee;                                               
 USE ieee.std_logic_1164.all;                                
 
-ENTITY write_back_vhd_tst IS
-END write_back_vhd_tst;
-ARCHITECTURE write_back_arch OF write_back_vhd_tst IS
+ENTITY write_back_tb IS
+END write_back_tb;
+ARCHITECTURE write_back_arch OF write_back_tb IS
 -- constants                                                 
 -- signals                                                   
 SIGNAL I_alu : STD_LOGIC_VECTOR(31 DOWNTO 0);
 SIGNAL I_branch : STD_LOGIC;
 SIGNAL I_clk : STD_LOGIC;
-SIGNAL I_en : STD_LOGIC;
+SIGNAL I_en : STD_LOGIC := '0';
 SIGNAL I_jump : STD_LOGIC;
 SIGNAL I_mem : STD_LOGIC_VECTOR(31 DOWNTO 0);
 SIGNAL I_mem_read : STD_LOGIC;
@@ -53,6 +26,7 @@ SIGNAL O_rd : STD_LOGIC_VECTOR(4 DOWNTO 0);
 SIGNAL O_we : STD_LOGIC;
 SIGNAL O_datas :  STD_LOGIC_VECTOR (31 downto 0);
 SIGNAL O_datat :  STD_LOGIC_VECTOR (31 downto 0);
+
 COMPONENT write_back
 	PORT (
 	I_alu : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
@@ -129,17 +103,20 @@ BEGIN
 END PROCESS;
 init : PROCESS                                               
 -- variable declarations                                     
-BEGIN                                                        
+BEGIN                
+	I_en <= '1';                                        
         -- code that executes only once                      
 	I_reset <= '1';
 	I_jump <= '0';
 	I_branch <= '0';
 	I_stall <= '0';
-	WAIT FOR 5*clk_period;
+
+	WAIT FOR clk_period;
 	I_reset <= '0';
 	I_en <= '0';
-	WAIT FOR 5*clk_period;
+	WAIT FOR clk_period;
 	
+	report "Test 1";
 	I_en <= '1';
 	I_regDwe <= '1';
 	I_mem_read <= '0';
@@ -147,57 +124,68 @@ BEGIN
 	I_mem <= "00000000000000000000000000000001";
 	I_rd <= "00001";
 	
-	WAIT FOR 5*clk_period;
+	WAIT FOR clk_period;
 	assert (O_mux = I_alu) report "Test 1: O_mux != I_alu" severity error;
-	WAIT FOR 5*clk_period;
+
+	WAIT FOR clk_period;
+	
+	report "Test 2";
 	I_datad <= I_alu;
 	I_rs <= I_rd;
 	I_we <= '1';
-	WAIT FOR 25*clk_period;
-	assert (O_mux = O_datas) report "Test 1: O_mux != O_datas" severity error;
-	WAIT FOR 5*clk_period;
-	
+	WAIT FOR clk_period;
+	assert (O_mux = O_datas) report "Test 2: O_mux != O_datas" severity error;
+
+
+	report "Test 3";
+	WAIT FOR clk_period;
+	report "Test 3";
 	I_jump <= '1';
-	WAIT FOR 5*clk_period;
-	assert (O_mux = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX") report "Jump Test: O_mux != Reset Value" severity error;
+	WAIT FOR clk_period;
+	assert (O_mux = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX") report "Test 3 Jump: O_mux != Reset Value" severity error;
 	I_jump <= '0';
-	I_en <= '0';
-	WAIT FOR 5*clk_period;
-	
+
+	WAIT FOR clk_period;
+
+	report "Test 4";
 	I_en <= '1';
 	I_regDwe <= '1';
 	I_mem_read <= '1';
 	I_rd <= "00010";
-	WAIT FOR 5*clk_period;
-	assert (O_mux = I_mem) report "Test 2: O_mux != I_mem" severity error;
-	WAIT FOR 5*clk_period;
+	WAIT FOR clk_period;
+	assert (O_mux = I_mem) report "Test 4: O_mux != I_mem" severity error;
+
+	
+	report "Test 5";
+	WAIT FOR clk_period;
 	I_datad <= I_mem;
 	I_rs <= I_rd;
 	I_we <= '1';
-	WAIT FOR 25*clk_period;
-	assert (O_mux = O_datas) report "Test 2: O_mux != O_datas" severity error;
-	WAIT FOR 5*clk_period;
-	
+	WAIT FOR clk_period;
+	assert (O_mux = O_datas) report "Test 5: O_mux != O_datas" severity error;
+	WAIT FOR clk_period;
+
+	report "Test 6";
 	I_branch <= '1';
-	WAIT FOR 5*clk_period;
-	assert (O_mux = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX") report "Branch: O_mux != Reset Value" severity error;
+	WAIT FOR clk_period;
+	assert (O_mux = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX") report "Test 6 Branch: O_mux != Reset Value" severity error;
 	I_branch <= '0';
-	I_en <= '0';
-	WAIT FOR 5*clk_period;
+	WAIT FOR clk_period;
 	
-	I_en <= '1';
+	report "Test 7";
 	I_regDwe <= '0';
 	I_mem_read <= '1';
 	I_rd <= "00011";
-	WAIT FOR 5*clk_period;
-	assert (O_mux = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX") report "Test 3: O_mux != Invalid input" severity error;
-	WAIT FOR 5*clk_period;
+	WAIT FOR clk_period;
+	assert (O_mux = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX") report "Test 7: O_mux != Invalid input" severity error;
+	wait for clk_period;
 	
+	report "Test 8";
 	I_stall <= '1';
-	WAIT FOR 5*clk_period;
-	assert (O_mux = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX") report "Stall: O_mux != Reset Value" severity error;
+	WAIT FOR clk_period;
+	assert (O_mux = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX") report "Test 8 Stall: O_mux != Reset Value" severity error;
 	
 WAIT;                                                       
-END PROCESS init;                                           
+END PROCESS;                                           
                                          
 END write_back_arch;
